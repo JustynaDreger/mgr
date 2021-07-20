08a73845ebb8dfecbadd4f5594da7f16f4d72ec3
JustynaDreger <draegerjusti@gmail.com>
2021-07-20 22:51:51 +0200

    Framework changed, Windows paint

---
+++ badanie/main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 20 20:38:23 2021
+
+@author: justyna
+"""
+"""
+przygotuje Pan skrypt, którego zadaniem będzie odtworzenie wybranych
+ filmów reklamowych. Między filmami powinien być ekran, z kilkoma etykietami
+ emocji, spośród których uczestnik powinien wskazać te, które jego zdaniem wywołał dany film.
+ Po nim powinien następować 2-gi ekran w którym zadaje Pani 3 pytania testowy na temat jakiś 
+ szczegółów, które pojawiły się na filmie. Odpowiedzi poprawne powinny być zliczane, 
+ a na zakończenie całego skryptu uczestnikowi powinna wyświetlać się informacja z jego wynikiem. 
+ W celach motywacyjnych będzie Pani musiała przygotować sobie do badania np. zestaw cukierków i 
+ wyraźnie na początku poinformować uczestnika, że musi  dokładnie oglądać filmy, 
+ żeby odpowiedzieć na jak największą liczbę pytać i tym samym otrzymać nagrodę. 
+ Oczywiście trzeba też ustalić, że np. za wszystkie bezbłędne odpowiedzi są 3 cukierki, 
+ za dwie błędne 2, za 4 błędne 1, a za więcej - brak nagrody. 
+ Przed rozpoczęciem 1-szego filmu potrzebujemy jeszcze zarejestrować sygnał w trakcie relaksu uczestnika, 
+ więc w ciągu pierwszej minuty na ekranie musi być jakaś informacja o relaksie z zamkniętymi oczami, 
+ w ciągu kolejnej minuty relaks z otwartymi oczami (przez całe dwie minuty powinna być jakaś relaksująca muzyka). 
+ W trakcie każdej zmiany ekranu będzie Pani przesyłała informacje o zdarzeniu do systemu rejestrującego sygnał EEG,
+ ale o tym napiszę Pani później jak przygotuje Pani skrypt.
+"""
+if __name__== '__main__':
+    pass
